4dd376dbdb8a2ecdf1422526e8dd6073cd627b09
Dmitrii Grishin <dmigrishin@gmail.com>
2019-05-27 02:24:06 +1000

    some changes after comments

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import User
+import config
 
 # Настройки прокси
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
@@ -7,13 +9,13 @@ PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
 import logging
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO,
+                    level=logging.DEBUG,
                     filename='bot.log'
                     )
 
 def main():
-    mybot = Updater("793650499:AAHmpenNWfX7_AKeYDDZZqCf6X83V_a6oKY", request_kwargs=PROXY)
-
+    mybot = Updater(config.SECRET_KEY, request_kwargs=PROXY)
+    
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
@@ -21,8 +23,17 @@ def main():
     mybot.idle()
 
 def greet_user(bot, update):
-    text = 'Вызван /start'
+    user = User
+    username = user.name
+    text = 'Привет {} Вызван {} /start'.format(username, bot.username)
     print(text)
+    
+    logging.debug (vars(user))
+    logging.debug(bot)
+    logging.debug("This is a debug message")
+    logging.info(text)
+    logging.info("Informational message")
+    logging.error("An error has happened!")
     update.message.reply_text(text)
 
 def talk_to_me(bot, update):
